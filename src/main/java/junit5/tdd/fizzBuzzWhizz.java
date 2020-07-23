91ad8bd77e80fe9f0f7d04a08cb7a762283ff2ae
Albert <Albert.c.x.chen@oocl.com>
2020-07-23 12:14:00 +0800

    [Albert] refactor : refactor FizzBuzzWhizz_play mention

--- src/main/java/junit5/tdd/fizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/fizzBuzzWhizz.java
@@ -1,22 +1,22 @@
 package junit5.tdd;
 
 public class fizzBuzzWhizz {
-    public String play(int number){
-         String result ="";
+    public String play(int number) {
+        String result = "";
 
-         if (number % 3 == 0) {
-             result+="Fizz";
-         }
-         if(number%5==0){
-             result+="Buzz";
-         }
-         if(number%7==0){
-             result+="Whizz";
-         }
+        if (number % 3 == 0) {
+            result += "Fizz";
+        }
+        if (number % 5 == 0) {
+            result += "Buzz";
+        }
+        if (number % 7 == 0) {
+            result += "Whizz";
+        }
 
         if (result.equals("")) {
             result = String.valueOf(number);
         }
-    return result;
+        return result;
     }
 }
--- src/test/java/junit5/tdd/fizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/fizzBuzzWhizzTest.java
@@ -11,67 +11,67 @@ public class fizzBuzzWhizzTest {
     @Test
     void should_1_when_fizzBuzzWhizz_play_given_1() {
         //given
-        int number =1;
+        int number = 1;
         String actual;
         //when
-        fizzBuzzWhizz fbz =new fizzBuzzWhizz();
-        actual=fbz.play(number);
+        fizzBuzzWhizz fbz = new fizzBuzzWhizz();
+        actual = fbz.play(number);
         //then
-        assertEquals("1",actual);
+        assertEquals("1", actual);
     }
 
     @Test
     void should_Fizz_when_fizzBuzzWhizz_play_given_3() {
         //given
-        int number =3;
+        int number = 3;
         String actual;
         //when
-        fizzBuzzWhizz fbz =new fizzBuzzWhizz();
-        actual=fbz.play(number);
+        fizzBuzzWhizz fbz = new fizzBuzzWhizz();
+        actual = fbz.play(number);
         //then
-        assertEquals("Fizz",actual);
+        assertEquals("Fizz", actual);
     }
 
     @Test
     void should_Buzz_when_fizzBuzzWhizz_play_given_5() {
         //given
-        int number =5;
+        int number = 5;
         String actual;
 
         //when
-        fizzBuzzWhizz fbz =new fizzBuzzWhizz();
-        actual=fbz.play(number);
+        fizzBuzzWhizz fbz = new fizzBuzzWhizz();
+        actual = fbz.play(number);
 
         //then
-        assertEquals("Buzz",actual);
+        assertEquals("Buzz", actual);
     }
 
     @Test
     void should_Whizz_when_fizzBuzzWhizz_play_given_7() {
         //given
-        int number =7;
+        int number = 7;
         String actual;
 
         //when
-        fizzBuzzWhizz fbz =new fizzBuzzWhizz();
-        actual=fbz.play(number);
+        fizzBuzzWhizz fbz = new fizzBuzzWhizz();
+        actual = fbz.play(number);
 
         //then
-        assertEquals("Whizz",actual);
+        assertEquals("Whizz", actual);
     }
 
     @Test
     void should_FizzBuzzWhizz_when_fizzBuzzWhizz_play_given_105() {
         //given
-        int number =105;
+        int number = 105;
         String actual;
 
         //when
-        fizzBuzzWhizz fbz =new fizzBuzzWhizz();
-        actual=fbz.play(number);
+        fizzBuzzWhizz fbz = new fizzBuzzWhizz();
+        actual = fbz.play(number);
 
         //then
-        assertEquals("FizzBuzzWhizz",actual);
+        assertEquals("FizzBuzzWhizz", actual);
     }
 
 }
